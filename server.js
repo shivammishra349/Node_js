5969e8f48e955e33e7b4f298172548328fc88c77
shivammishra349 <shivam9.mishra1999@gmail.com>
2024-01-12 10:34:22 +0530

    fetchind data from url

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-let http=require("http");
+let http=require('http');
 let server=http.createServer((req,res)=>{
-    res.end("shivam")
+    let url=req.url
+    
+    if(url==='/home'){
+        return res.end('welcome to home')
+    }
+    if(url==='/about'){
+        return res.end('wlcome to about us page')
+    }
+    if(url==='/node'){
+        return res.end('Welcome to my Node Js project')
+    }
+    res.end('welcome to my node js project')
 })
-
-server.listen(4000);
+server.listen(9000,()=>{
+    console.log("server is on")
+})
